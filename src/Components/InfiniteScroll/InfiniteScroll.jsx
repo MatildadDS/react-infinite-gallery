2e0f049ecccba18dac62530b71dc9676b3070bf7
Matildad <mtld.desouza@gmail.com>
2021-05-31 14:52:17 +0200

    first API call:tada:

--- src/Components/InfiniteScroll/InfiniteScroll.jsx
+++ src/Components/InfiniteScroll/InfiniteScroll.jsx
@@ -8,6 +8,46 @@ export default function InfiniteScroll() {
     const [dataImg, setDataImg] = useState([[], [],[]]) 
     const [pageIndex, setPageIndex] = useState(1)
     const [searchState, setSearchState] = useState('random')
+
+    const infiniteFetchData = () => {
+
+        fetch(`https://api.unsplash.com/search/photos?page=${pageIndex}&per_page=30&query=${searchState}&client_id=uQZqRt9wXrpoSLoTXvPsxnkMn31QNRmSbsu2V8_acnM`)
+        .then((response) => {
+            return response.json()
+        })
+        .then((data) => {
+
+            const imgsReceived = [];
+
+            data.results.forEach((img) => {
+                imgsReceived.push(img.urls.regular)
+            })
+
+            const newFreshState =[
+                [...dataImg[0]],
+                [...dataImg[1]],
+                [...dataImg[2]],
+            ]
+
+            let index = 0;
+            for(let i = 0; i < 3; i++){
+                for(let j = 0; j < 10; j++){
+                    newFreshState[i].push(imgsReceived[index])
+                    index++;
+                }
+            }
+
+            setDataImg(newFreshState)
+        })
+    }
+
+    console.log(dataImg);
+
+    useEffect(() => {
+
+        infiniteFetchData();
+        
+    }, [])
  
     const handleSearch = e => {
         e.preventDefault()
@@ -22,9 +62,21 @@ export default function InfiniteScroll() {
                 <input type="text" id="search" ref={inpRef}/>
             </form>
             <div className="card-list">
-                <div></div>
-                <div></div>
-                <div></div>
+                <div>
+                    {dataImg[0].map(img => {
+                        return <img key={uuidv4()} src={img} alt="unsplash images"/>
+                    })}
+                </div>
+                <div>
+                {dataImg[1].map(img => {
+                        return <img key={uuidv4()} src={img} alt="unsplash images"/>
+                    })}
+                </div>
+                <div>
+                {dataImg[2].map(img => {
+                        return <img key={uuidv4()} src={img} alt="unsplash images"/>
+                    })}
+                </div>
             </div>
         </div>
     )
